f962295765da14743a1bc3919941b8009423bff2
Ivan Yuriev <Ivan.Yuriev@avionero.com>
2020-12-11 20:51:01 +0300

    TODO for the next step

--- Checkers.Core/Game/Game.cs
+++ Checkers.Core/Game/Game.cs
@@ -10,6 +10,12 @@ using System.Threading.Tasks;
 
 namespace Checkers.Core.Game
 {
+
+
+    //TODO: Game object should be active or passive (push or pull interaction with Player object)???
+    //TODO: How to limit time of move - for AI, for Player?
+    //TODO: include results table and history tracking - list of moves in a proper formatting
+
     public class Game
     {
         private readonly IRules _rulesProvider;
